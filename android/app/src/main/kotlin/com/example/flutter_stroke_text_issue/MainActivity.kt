1bb3f3599cc81c89ce26278b8a65f0bbe98dd973
xmk <289203664@qq.com>
2022-09-22 13:13:00 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_stroke_text_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_stroke_text_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/text.dart
@@ -0,0 +1,69 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+
+class PostTextPainter extends CustomPainter {
+  final Paint p = Paint();
+  final Color textColor = Colors.white;
+  final double fontSize = 100;
+  final TextPainter tp = TextPainter(
+    textDirection: TextDirection.ltr,
+  );
+  final bool drawBody;
+  final bool drawStroke;
+
+  PostTextPainter({this.drawBody = true, this.drawStroke = false}) {
+    p.strokeCap = StrokeCap.square;
+    p.strokeWidth = 1;
+  }
+
+  @override
+  bool? hitTest(ui.Offset position) {
+    // return super.hitTest(position);
+    return false;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    drawText(canvas, size);
+  }
+
+  void drawText(Canvas canvas, Size size) {
+    if (drawStroke) {
+      // draw text stroke
+      p.color = Colors.black;
+      p.style = PaintingStyle.stroke;
+      p.strokeWidth = 1;
+      drawTextInternal(canvas, size);
+    }
+    if (drawBody) {
+      // draw text body
+      p.color = textColor;
+      p.style = PaintingStyle.fill;
+      drawTextInternal(canvas, size);
+    }
+  }
+
+  void drawTextInternal(Canvas canvas, Size size) {
+    tp.text = TextSpan(
+      text: "Test",
+      style: TextStyle(
+        fontSize: fontSize,
+        // fontFamily: fontFamily,
+        height: 1,
+        foreground: p,
+      ),
+    );
+    tp.layout(
+      // minWidth:0,
+      minWidth: 200,
+      maxWidth: 200,
+    );
+    tp.paint(canvas, Offset(-100, -100));
+  }
+
+  @override
+  bool shouldRepaint(PostTextPainter oldDelegate) {
+    return this != oldDelegate;
+  }
+}
